b62898b0ebca6bb8e846a887419abe716e3d7f79
Gulzina Pakzat <gulzine@gmail.com>
2020-04-12 14:48:38 -0700

    added first class

---
+++ src/test/java/tests/FirstClass.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi world");
+        System.out.println("I am working until 5");
+        System.out.println("i am working until 5");
+    }
+}
---
+++ src/test/java/tests/SecondClass.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        String myName="Humayra";
+        System.out.println(myName);
+        System.out.println("this is another line");
+    }
+
+
+}
+
