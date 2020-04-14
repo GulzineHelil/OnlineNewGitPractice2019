f246daad25d1cfb2e61027a51c7ac3c38daccdd8
GulzineHelil <61639447+GulzineHelil@users.noreply.github.com>
2020-04-14 11:50:20 -0700

    Update SecondClass.java

--- src/test/java/tests/SecondClass.java
+++ src/test/java/tests/SecondClass.java
@@ -2,7 +2,7 @@ package tests;
 
 public class SecondClass {
     public static void main(String[] args) {
-        String myName="Humayra";
+        String myName="Luis patron";
         System.out.println(myName);
         System.out.println("this is another line");
           System.out.println("this is another line");
