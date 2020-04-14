2824d3ae3d2d647c4659c7be3acb49fd8e8d2395
Gulzina Pakzat <gulzine@gmail.com>
2020-04-14 11:52:21 -0700

    change a line

--- src/test/java/tests/FirstClass.java
+++ src/test/java/tests/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
     public static void main(String[] args) {
 
+
         System.out.println("Hi everyone");
         System.out.println("I am working until 5");
 
--- src/test/java/tests/SecondClass.java
+++ src/test/java/tests/SecondClass.java
@@ -2,10 +2,10 @@ package tests;
 
 public class SecondClass {
     public static void main(String[] args) {
-        String myName="Humayra";
+        String myName="John B Goodenough";
         System.out.println(myName);
         System.out.println("this is another line");
-          System.out.println("this is another line");
+        System.out.println("this is another line");
         System.out.println("this is another line");
         
     }
